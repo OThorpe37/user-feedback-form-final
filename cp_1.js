9d5c85ec2e64c57ca501e5bbf5bbe38d52f8faa3
Olivia Thorpe <othorpe@usf.edu>
2026-03-10 23:53:24 -0400

    Added Character Count Function

--- cp_1.js
+++ cp_1.js
@@ -3,3 +3,9 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const commentsInput = document.getElementById("comments");
 const feedbackDisplay = document.getElementById("feedback-display");
+
+const charCount = document.getElementById("char-count");
+commentsInput.addEventListener("input", function() {
+    let count = commentsInput.value.length;
+    charCount.textContent = count + " characters";
+});
