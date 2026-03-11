f357b3a58049fa187d3bc07a439839336395fe49
Olivia Thorpe <othorpe@usf.edu>
2026-03-10 23:49:58 -0400

    Added Initial DOM API elements to JS

---
+++ cp_1.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("feedback-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const commentsInput = document.getElementById("comments");
+const feedbackDisplay = document.getElementById("feedback-display");
